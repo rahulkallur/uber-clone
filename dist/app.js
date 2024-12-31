dee79a7e0cc10d0dff61a4152ca5d13f09a5552f
rahulkallur <rahulkallur63@gmail.com>
2024-12-31 11:00:07 +0530

    added test scripts

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,16 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 const app = express();
 app.use(cors());
+app.use(morgan("dev"));
 app.get("/", (req, res) => {
+    for (let i = 0; i < 100000000; i++) { }
     res.send("Hello World!");
+});
+app.get("/stress-test", (req, res) => {
+    for (let i = 0; i < 100000000; i++) { }
+    res.send("Stress Test!");
 });
 app.listen("3000", () => {
     console.log("Server is running on port 3000");
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 app.use(cors());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
+  for(let i = 0; i < 100000000; i++) {}
   res.send("Hello World!");
+});
+
+app.get("/stress-test", (req: Request, res: Response) => {
+  for(let i = 0; i < 100000000; i++) {}
+  res.send("Stress Test!");
 });
 
 app.listen("3000", () => {
